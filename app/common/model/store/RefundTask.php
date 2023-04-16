5c21243eebff3af2785da1cede32512cf7283e02
x <895740313@qq.com>
2023-04-16 14:30:33 +0800

    添加 $this->setAttr('err_msg', $msg);

--- app/common/model/store/RefundTask.php
+++ app/common/model/store/RefundTask.php
@@ -32,11 +32,13 @@ class RefundTask extends BaseModel
     {
         # 如果没有错误的话那么返回false继续向下执行
         if (empty($errArr)) return false;
-        Log::debug("退款错误获取到参数 {$this->getAttr('refund_task_id')} " . implode(";", array_merge(explode(";", $this->getAttr('err_msg')) ?? [], $errArr)));
+        $msg = implode(";", array_merge(explode(";", $this->getAttr('err_msg') ?? ''), $errArr));
+        Log::debug("退款错误获取到参数 {$this->getAttr('refund_task_id')} " . $msg);
         # 解析先前存在的错误
         RefundTask::getInstance()->where('refund_task_id', $this->getAttr('refund_task_id'))->update([
-            'err_msg' => implode(";", array_merge(explode(";", $this->getAttr('err_msg') ?? ''), $errArr))
+            'err_msg' => $msg
         ]);
+        $this->setAttr('err_msg', $msg);
         # 变更退款状态
         StoreRefundOrder::getInstance()->where('refund_order_id', $this->getAttr('refund_order_id'))->update([
             'status' => 5
