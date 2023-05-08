e9846a54a033e5c09ca3e7295e6884e5af74d53a
nihao <18829062526@163.com>
2023-05-08 11:56:10 +0800

    获取用户

--- app/controller/api/Auth.php
+++ app/controller/api/Auth.php
@@ -544,8 +544,7 @@ class Auth extends BaseController
                     $openPlatformRepository = app()->make(OpenPlatformRepository::class);
 
                     $userInfoCong =  $openPlatformRepository->thirdpartyCode2Session($appid,$code);
-                    var_dump($userInfoCong);
-                    if (!isset($userInfoCong['unionid'])) throw new ValidateException('授权失败,参数有误');
+                    if (!isset($userInfoCong['session_key'])) throw new ValidateException('授权失败,参数有误');
 
 
                     Cache::set('eb_api_code_' . $code, $userInfoCong, 86400);
@@ -871,11 +870,11 @@ class Auth extends BaseController
 
 
         $data =  $openPlatformRepository->thirdpartyCode2Session($appid,$js_code);
-        if (!isset($data['unionid']))return app('json')->status(400,'授权失败');
+        if (!isset($data['session_key']))return app('json')->status(400,'授权失败');
 
 
         //查询wechat_user_id
-        $wechat_user_id = WechatUser::getDB()->where("unionid","=",$data['unionid'])->value("wechat_user_id");
+        $wechat_user_id = WechatUser::getDB()->where("routine_openid","=",$data['openid'])->value("wechat_user_id");
         if (!$wechat_user_id)   return app('json')->status(400,"用户不存在");
         $userRepository = app()->make(UserRepository::class);
         $user = $userRepository->wechatUserIdBytUser($wechat_user_id);
