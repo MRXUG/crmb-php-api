a6c2325792d9187ceeba3c16803dc74d3b970cb0
king <yewei@myhayo.com>
2023-06-20 23:07:14 +0800

    限制外网触发内部脚本

--- route/internal.php
+++ route/internal.php
@@ -2,8 +2,7 @@
 
 use think\facade\Route;
 
-//todo 请求IP的白名单
 Route::group('api/internal/', function () {
     Route::get('trigger/autoTakeOrder', 'api.internal.AutoTakeOrder/httpTrigger');
     Route::get('trigger/orderRefund', 'api.internal.OrderRefund/httpTrigger');
-});
+})->middleware(\app\common\middleware\InternalRequestMiddleware::class);
