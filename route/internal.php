49c4d3dbefc01abbc3f57f38b2d672220e679a2d
wxy <wxy@email.com>
2023-08-09 10:43:51 +0800

    fix

--- route/internal.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use think\facade\Route;
-
-Route::group('api/internal/', function () {
-    Route::get('trigger/autoTakeOrder', 'api.internal.AutoTakeOrder/httpTrigger');
-    Route::get('trigger/orderRefund', 'api.internal.OrderRefund/httpTrigger');
-})->middleware(\app\common\middleware\InternalRequestMiddleware::class);
