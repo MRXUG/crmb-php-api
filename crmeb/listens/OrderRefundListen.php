889193c27b0274b8279c52482b71626bda87889c
lucky-xu <1039147110@qq.com>
2023-08-22 19:35:38 +0800

    分账作为押金属性，会由售后押款统一回退 此处不需要重复回退

--- crmeb/listens/OrderRefundListen.php
+++ crmeb/listens/OrderRefundListen.php
@@ -144,6 +144,11 @@ class OrderRefundListen extends TimerService implements ListenerInterface
         $merId = $item->getAttr('mer_id');
         // 获取商户配置
         $make   = WechatService::getMerPayObj($merId, $item->getAttr('app_id'));
+        // 查询回退结果 原始逻辑
+        // 分账作为押金属性，会由售后押款统一回退 此处不需要重复回退，直接查询即可  ProfitSharing::createRefundTask
+        return $make->profitSharing()
+            ->profitSharingReturnResult($orderSn, $orderSn);
+
         /** @var DeliveryProfitSharingStatusPart $profitSharingPartRepos */
         $profitSharingPartRepos = app()->make(DeliveryProfitSharingStatusPartRepository::class);
         switch ($info['platform_source']){
